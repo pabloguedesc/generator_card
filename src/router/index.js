071c6b1c709a8f9f15176947e532882747df6034
Pablo <pabloguedesc@gmail.com>
2021-11-04 15:55:31 -0400

    updated router main

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import("../views/Home.vue")
-  },
-  {
-    path: '/card',
     name: 'Card',
     component: () => import("../components/CardMain.vue")
   }
